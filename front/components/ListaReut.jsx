fdd97762f2adb1ff4bf14940c585a76c1e528cb1
amatana <amatana95@gmail.com>
2019-09-10 05:31:31 -0300

    comiteando la lista

---
+++ front/components/ListaReut.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const List = ({ list = [], title }) => (
+  <div className="paddingGral">
+    <h2 className="listTitle">{title || "List Title"}</h2>
+
+    {list.map((item, key) => (
+      <div key={key++} className="gridPhoto">
+        <div className="roundedPhoto colorTrial" />
+        <div className="textList">
+          <h2>Nombre de la Cosa</h2>
+          <p>Especialidad</p>
+        </div>
+      </div>
+    ))}
+  </div>
+);
+
+export default List;
