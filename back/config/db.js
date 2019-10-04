54b17f4feb656c8dcb7e63066dcc6c57a4ba245c
Maxi <tholem98@gmail.com>
2019-10-04 17:06:48 -0300

    trying to solve the issue of clientID

--- back/config/db.js
+++ back/config/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const dotenv = require('dotenv')
+// const dotenv = require('dotenv')
 dotenv.config()
 
 const sequelize = new Sequelize('postgres://localhost:' +process.env.PG_PORT + '/dbpartners', {logging : false})
